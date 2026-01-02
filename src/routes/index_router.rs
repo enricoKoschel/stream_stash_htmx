e5ce53672ae50f036cce0337b6a46123fbb5987c
Enrico Koschel <enricokoschel02@gmail.com>
2026-01-02 04:02:08 +0100

    Add card count endpoint for testing

--- src/routes/index_router.rs
+++ src/routes/index_router.rs
@@ -1,12 +1,15 @@
 use crate::views::components::MediaCard;
 use crate::views::maybe_document;
 use crate::views::pages::{about_page, main_page};
+use axum::extract::Path;
 use axum::response::IntoResponse;
 use axum::{Router, routing::get};
 use axum_htmx::HxRequest;
 use std::iter;
 
-async fn index(HxRequest(hx_request): HxRequest) -> impl IntoResponse {
+async fn index(HxRequest(hx_request): HxRequest, path: Option<Path<usize>>) -> impl IntoResponse {
+    let count = path.map_or(50, |p| p.0);
+
     let media: Vec<MediaCard> = iter::repeat_with(|| [
         MediaCard {
             title: "Harry Potter and the Philosopher's Stone",
@@ -20,7 +23,7 @@ async fn index(HxRequest(hx_request): HxRequest) -> impl IntoResponse {
             poster_url: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/ztkUQFLlC19CCMYHW9o1zWhJRNq.jpg",
             media_page_url: "/media/tv/1",
         },
-    ]).flatten().take(50).collect();
+    ]).flatten().take(count).collect();
 
     maybe_document(hx_request, main_page(&media))
 }
@@ -32,5 +35,6 @@ async fn about(HxRequest(hx_request): HxRequest) -> impl IntoResponse {
 pub fn index_router() -> Router {
     Router::<()>::new()
         .route("/", get(index))
+        .route("/{count}", get(index))
         .route("/about", get(about))
 }
