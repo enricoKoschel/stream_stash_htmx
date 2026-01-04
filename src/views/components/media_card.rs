f6e2379b22508e130f93ef60e2aff0667aa7f2f7
Enrico Koschel <enricokoschel02@gmail.com>
2026-01-04 01:47:05 +0100

    Add media card for mobile

--- src/views/components/media_card.rs
+++ src/views/components/media_card.rs
@@ -8,13 +8,21 @@ pub fn media_card<'a>(
     media_page_url: &'a str,
 ) -> impl Renderable {
     maud! {
-        a class="relative group" href=media_page_url {
+        a class="relative group pointer-coarse:hidden" href=media_page_url {
             img class="rounded-lg brightness-100 group-hover:brightness-28 transition-[filter]" src=poster_url;
 
-            div class="absolute inset-0 mx-3 flex flex-col justify-center opacity-0 group-hover:opacity-100 transition-[opacity]" {
-                h2 class="text-lg font-medium text-white text-center mb-3" { (title) }
-                p class="text-white text-md text-center" { (year) }
+            div class="absolute inset-0 m-4 flex flex-col justify-center opacity-0 group-hover:opacity-100 transition-[opacity]" {
+                h2 class="text-lg font-medium text-center mb-3 line-clamp-5 text-ellipsis wrap-anywhere " { (title) }
+                p class="text-md text-center" { (year) }
             }
+        }
+        a class="pointer-fine:hidden bg-gray-700 rounded-lg flex flex-col" href=media_page_url {
+            div class="grow flex flex-col justify-center m-2" {
+                h2 class="text-md font-medium text-center mb-3 line-clamp-3 text-ellipsis wrap-anywhere" { (title) }
+                p class="text-sm text-center" { (year) }
+            }
+
+            img class="rounded-b-lg" src=poster_url;
         }
     }
 }
